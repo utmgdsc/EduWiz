41fd8ac225da82558b6306001bc437750274f6cb
Luis V <pateayt@gmail.com>
2025-01-29 18:56:39 -0500

    Ensure that logs directory exists when setting up logger

--- backend/server/logger.py
+++ backend/server/logger.py
@@ -1,13 +1,19 @@
 import logging
+import os
 
 
 def setup_logger():
+    log_dir = "./logs"
+    log_file = os.path.join(log_dir, "app.log")
+
+    os.makedirs(log_dir, exist_ok=True)
+
     logging.basicConfig(
         format="%(name)s - %(levelname)s - %(asctime)s - %(message)s",
         level=logging.DEBUG,
     )
 
-    file_handler = logging.FileHandler(filename="./logs/app.log", encoding="utf-8")
+    file_handler = logging.FileHandler(filename=log_file, encoding="utf-8")
     file_handler.setFormatter(
         logging.Formatter("%(name)s - %(levelname)s - %(asctime)s - %(message)s")
     )

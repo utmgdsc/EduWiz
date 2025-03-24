fdd3a259a064c0daba2162fe4b27fbc25670f61b
MusaKhan16 <mmusa.akhan@gmail.com>
2025-03-23 23:59:13 -0400

    chat endpoint wip

---
+++ backend/server/routes/chat.py
@@ -0,0 +1,69 @@
+import pydantic
+import pathlib
+import functools
+
+from typing import Literal
+from fastapi import APIRouter, Depends
+
+from langchain_openai import ChatOpenAI
+
+from langchain_core.language_models.chat_models import BaseChatModel
+
+from server.lib.auth import FirebaseAuthMiddleware
+from server.lib.auth.invariant import email_is_verified
+
+router = APIRouter(
+    prefix="/chat",
+    tags=["chat"],
+    dependencies=[Depends(FirebaseAuthMiddleware(email_is_verified))],
+)
+
+
+async def get_llm() -> BaseChatModel:
+    """Chosen language Model gpt-4o-mini for testing purposes"""
+    return ChatOpenAI(model="gpt-4o-mini-2024-07-18")
+
+
+@functools.cache
+async def get_system_prompt() -> str:
+    with open(
+        pathlib.Path(__file__).parent / "lib" / "prompts" / "chat_system.txt"
+    ) as f:
+        return f.read()
+
+
+class Message(pydantic.BaseModel):
+    type: Literal["system", "user", "assistant"]
+    content: str
+
+
+class VideoChatContext(pydantic.BaseModel):
+    prompt: str
+    initial_prompt: str
+    video_context: str
+    messages: list[Message]
+
+
+# Create langchain helpers
+# Create streaming responses
+@router.post("/message_video", response_model=Message)
+async def message_video(
+    chat_history: VideoChatContext,
+    llm: BaseChatModel = Depends(get_llm),
+    system_prompt: str = Depends(get_system_prompt),
+):
+    """Respond to the user with chat context"""
+    system_message = Message(
+        type="system",
+        content=system_prompt.format(
+            initial_prompt=chat_history.initial_prompt,
+            context=chat_history.video_context,
+        ),
+    )
+
+    messages = list(
+        map(Message.model_dump, [system_message] + chat_history.messages[:30])
+    )
+
+    result = await llm.ainvoke(messages)
+    return result

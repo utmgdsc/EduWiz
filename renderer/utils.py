76c1ad8f3a66d6401eaceca89780204d926f4ef3
varoniba <luis.varoni@mail.utoronto.ca>
2025-03-24 15:08:32 -0400

    Merged two previous branches

---
+++ renderer/utils.py
@@ -0,0 +1,13 @@
+import re
+from typing import AsyncIterable
+import logging
+
+logger = logging.getLogger("eduwiz.utils")
+JOB_LIMIT = 10
+
+def count_total_animations_in_code(code: str) -> int:
+    """Count the total number of animations in the Manim code."""
+    play_count = len(re.findall(r"self\.play\(", code))
+    wait_count = len(re.findall(r"self\.wait\(", code))
+    total = play_count + wait_count
+    return total

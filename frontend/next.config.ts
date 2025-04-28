4773b16812161fdc25aaddb45ac51775312c2648
MusaKhan16 <mmusa.akhan@gmail.com>
2025-04-28 12:31:13 -0400

    Disabling eslint for experiemnt

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,14 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      'three': require.resolve('three'),
+      three: require.resolve("three"),
     };
     return config;
   },
-
 };
 
 export default nextConfig;
